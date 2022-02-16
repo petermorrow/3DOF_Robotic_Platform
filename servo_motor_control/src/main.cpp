cb0bb7f02643de97ad9a3bb295fce086561f8cb1
Peter Morrow <petermorrow1998@gmail.com>
2022-02-16 16:03:32 +0000

    Mapping servo angles to min and max Duty cycles

--- servo_motor_control/src/main.cpp
+++ servo_motor_control/src/main.cpp
@@ -1,8 +1,8 @@
-// Initial test of servo motor control code 
-// Test operation of savox SA1258TG servo
+// Servo motor control code 
+// Operation of savox SA1258TG servo
 // PWM signal frequency set manually
 // Desired micro seconds specified from calculated Duty cycle
-// test change code
+
 
 #include <Arduino.h>
 
@@ -13,34 +13,52 @@ int servo_4 = 20;
 int LED = 13;
 int pos = 100;
 
-double test_angle = 127.872;
 
-double test_angle_2 = 120;
+double min_duty_cycle = 76.8; //Min duty cycle (0 degrees) - calabration for table Max
+double max_duty_cycle = 179.2; // Max DC (130 degrees from table max height)
 
-double test_angle_3 = 60;
-double test_angle_4 = 120;
+int servo_angle = 10;
 
 void setup() {
-  pinMode(servo_1, OUTPUT); //PWM set as output pin
+  pinMode(servo_1, OUTPUT); //Servo 1 set as output pin
+  pinMode(servo_2, OUTPUT);
+  pinMode(servo_3, OUTPUT);
+  pinMode(servo_4, OUTPUT);
+
   analogWriteFrequency(servo_1, 333); //set desired operating frequency for savox SA1258TG servo
   // this will set every pin linked to FTM0 timer = 333Hz (Pin: 5, 6, 9, 10, 20, 21, 22, 23)
+
+  
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  analogWrite(servo_1, test_angle_2); // Nutral position - 1500 micro seconds
-  analogWrite(servo_2, test_angle_2); // Nutral position - 1500 micro seconds
-  analogWrite(servo_3, test_angle_2); // Nutral position - 1500 micro seconds
-  analogWrite(servo_4, test_angle_2); // Nutral position - 1500 micro seconds
-  delay(100);
+  //put your main code here, to run repeatedly:
 
-  analogWrite(servo_1, test_angle_3); // Nutral position - 1500 micro seconds
-  analogWrite(servo_2, test_angle_3); // Nutral position - 1500 micro seconds
-  analogWrite(servo_3, test_angle_3); // Nutral position - 1500 micro seconds
-  analogWrite(servo_4, test_angle_3); // Nutral position - 1500 micro seconds
-  delay(250);
+  // map recieved servo angle (0(max)-130(min)) to Duty cycle max an min values
+
+  servo_angle = 100;
+  int angle = map(servo_angle, 0, 130, min_duty_cycle, 179.2);
+  analogWrite(servo_1, angle); // Nutral position - 1500 micro seconds
+  analogWrite(servo_2, angle); // Nutral position - 1500 micro seconds
+  analogWrite(servo_3, angle); // Nutral position - 1500 micro seconds
+  analogWrite(servo_4, angle); // Nutral position - 1500 micro seconds
+  delay(300);
+  
 
 }
 
 
+
+// step between Max and min values
+
+// for(servo_angle = 0; servo_angle<=130; servo_angle++){
+//     int angle = map(servo_angle, 0, 130, 76.8, 179.2);
+//     analogWrite(servo_1, angle); // Nutral position - 1500 micro seconds
+//     analogWrite(servo_2, angle); // Nutral position - 1500 micro seconds
+//     analogWrite(servo_3, angle); // Nutral position - 1500 micro seconds
+//     analogWrite(servo_4, angle); // Nutral position - 1500 micro seconds
+//     delay(10);
+
+//   }
 
