b0300a2d53d79fbeee7768a23354c3f72ddc104f
Peter Morrow <petermorrow1998@gmail.com>
2022-02-16 14:01:26 +0000

    First test commit

--- servo_motor_control/src/main.cpp
+++ servo_motor_control/src/main.cpp
@@ -2,6 +2,7 @@
 // Test operation of savox SA1258TG servo
 // PWM signal frequency set manually
 // Desired micro seconds specified from calculated Duty cycle
+// test change code
 
 #include <Arduino.h>
 
@@ -39,98 +40,7 @@ void loop() {
   analogWrite(servo_4, test_angle_3); // Nutral position - 1500 micro seconds
   delay(250);
 
-  // analogWrite(servo_1, 100); // Nutral position - 1500 micro seconds
-  // analogWrite(servo_2, 100); // Nutral position - 1500 micro seconds
-  // analogWrite(servo_3, 100); // Nutral position - 1500 micro seconds
-  // analogWrite(servo_4, 100); // Nutral position - 1500 micro seconds
-  // delay(1000);
-
-  // for(int i =0; i<=20; i++){
-  //   analogWrite(servo_1, pos); // Nutral position - 1500 micro seconds
-  //   analogWrite(servo_2, pos+i); // Nutral position - 1500 micro seconds
-  //   analogWrite(servo_3, pos); // Nutral position - 1500 micro seconds
-  //   analogWrite(servo_4, pos-i); // Nutral position - 1500 micro seconds
-  //   delay(100);
-  // }
-
-  // analogWrite(servo_1, 100); // Nutral position - 1500 micro seconds
-  // analogWrite(servo_2, 100); // Nutral position - 1500 micro seconds
-  // analogWrite(servo_3, 100); // Nutral position - 1500 micro seconds
-  // analogWrite(servo_4, 100); // Nutral position - 1500 micro seconds
-  // delay(1000);
-
-  // for(int i =0; i<=20; i++){
-  //   analogWrite(servo_1, pos); // Nutral position - 1500 micro seconds
-  //   analogWrite(servo_2, pos-i); // Nutral position - 1500 micro seconds
-  //   analogWrite(servo_3, pos); // Nutral position - 1500 micro seconds
-  //   analogWrite(servo_4, pos+i); // Nutral position - 1500 micro seconds
-  //   delay(100);
-  // }
-
-  // analogWrite(servo_1, 100); // Nutral position - 1500 micro seconds
-  // analogWrite(servo_2, 100); // Nutral position - 1500 micro seconds
-  // analogWrite(servo_3, 100); // Nutral position - 1500 micro seconds
-  // analogWrite(servo_4, 100); // Nutral position - 1500 micro seconds
-  // delay(1000);
-
-  // analogWrite(servo_1, 120); // Nutral position - 1500 micro seconds
-  // analogWrite(servo_2, 100); // Nutral position - 1500 micro seconds
-  // analogWrite(servo_3, 80); // Nutral position - 1500 micro seconds
-  // analogWrite(servo_4, 100); // Nutral position - 1500 micro seconds
-  // delay(1000);
-
-  // analogWrite(servo_1, 100); // Nutral position - 1500 micro seconds
-  // analogWrite(servo_2, 100); // Nutral position - 1500 micro seconds
-  // analogWrite(servo_3, 100); // Nutral position - 1500 micro seconds
-  // analogWrite(servo_4, 100); // Nutral position - 1500 micro seconds
-  // delay(1000);
-
-  // analogWrite(servo_1, test_angle_4); // Nutral position - 1500 micro seconds
-  // analogWrite(servo_2, test_angle_4); // Nutral position - 1500 micro seconds
-  // analogWrite(servo_3, test_angle_4); // Nutral position - 1500 micro seconds
-  // analogWrite(servo_4, test_angle_4); // Nutral position - 1500 micro seconds
-  // delay(2000);
-
-
-  // delay(5000);
-
-  // analogWrite(servo_1, 187.5456); // Max position - 2200 micro seconds
-  // delay(1000);
-  // analogWrite(servo_1, 68.1984); // Min position - 800 micro seconds
-  // delay(1000);
- 
 }
 
 
-
-// analogWrite(servo_1, 85.248); //  1000 micro seconds
-// delay(500);
-// analogWrite(servo_1, 161.9712); //  1900 micro seconds
-// delay(500);
-// analogWrite(servo_1, 170.496); //  2000 micro seconds
-// delay(500);
-
-
-// #include <Servo.h>
-// Servo servo1;
-// int servoPin = 23;
-// int pos = 1000;
-
-// void setup(){
-//   servo1.attach(servoPin);
-// }
-
-// void loop(){
-
-//   //servo1.writeMicroseconds(pos);
-//   servo1.writeMicroseconds(pos);
-//   for(pos =1000; pos<=2000; pos=pos+100){
-//     servo1.writeMicroseconds(pos);
-//     delay(100);
-//   }
-//   for(pos =2000; pos>=1000; pos=pos-100){
-//     servo1.writeMicroseconds(pos);
-//     delay(100);
-//   }
-// }
 
